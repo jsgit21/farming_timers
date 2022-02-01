cd9fca2d31ac8b62f9b5f5c89801290151594bf1
Joseph Santantonio <js958@njit.edu>
2022-02-01 16:02:05 -0500

    Added adjustable offset and reset button

--- farming_timers.py
+++ farming_timers.py
@@ -1,5 +1,6 @@
 from ast import Raise
 from asyncio.windows_events import NULL
+from cgitb import reset
 from doctest import master
 from re import X
 from textwrap import fill
@@ -11,15 +12,23 @@ from functools import partial
 import datetime
 import os
 
+# name: crop name
+# cycles: number of cycles it takes to fully grow
+# cycle_time: how long each cycle is
+# label_name: reference to the label that holds crop name
+# harvest_label: reference to the label that holds harvest time
+# remaining_label: reference to the label that holds remaining growth time
+# active: defines whether the timer is active already or not, so that it 
+#         cannot be triggered more than once
 allotments = [
-    {"name":"Potato", "cycles": 1, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Onion", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Cabbage", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Tomato", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Sweetcorn", "cycles": 6, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Strawberry", "cycles": 6, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Watermelon", "cycles": 8, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False},
-    {"name":"Snapegrass", "cycles": 7, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False}
+    {"name":"Potato", "cycles": 1, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Onion", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Cabbage", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Tomato", "cycles": 4, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Sweetcorn", "cycles": 6, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Strawberry", "cycles": 6, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Watermelon", "cycles": 8, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False},
+    {"name":"Snapegrass", "cycles": 7, "cycle_time":10, "label_name":NULL, "harvest_label":NULL, "remaining_label":NULL, "active":False, "reset":False}
 ]
 
 label_dict = {}
@@ -32,6 +41,7 @@ window.title("Farming timers")
 
 cwd = os.getcwd()
 icon_path = cwd + "\\farming_icons\\"
+offset = tk.IntVar() # Will hold Offset Value
 
 time_now = datetime.datetime.now()
 current_time = time_now.strftime('%I:%M %p')
@@ -53,54 +63,59 @@ def flatten_time():
         time_label.config(text=current_time)
         update_time()
 
+def reset_crops():
+    for i in range(len(allotments)):
+        crop = allotments[i]
+        if crop["active"]:
+            crop["reset"] = True # Also triggers active=Flase inside handle_growth_time
+            crop["label_name"].config(background="#F0F0F0")
+            crop["harvest_label"].config(text = "--:--", foreground = "grey", font='ariel 12')
+            crop["remaining_label"].config(text = "--:--", foreground = "grey", font='ariel 12')
+
 def calc_harvest_time(crop):
     # Eventually this offset must be recieved from the user on the GUI (0-15)
-    player_offset = 5
+    player_offset = offset.get()
 
     offset_mins = datetime.timedelta(minutes=player_offset)
     cycle_time = crop["cycle_time"]
     num_cycles = crop["cycles"]
 
-    #print("Crop: " + str(crop))
     crop_growtime = num_cycles*cycle_time
-    #print("crop_growtime: " + str(crop_growtime))
     time_change = datetime.timedelta(minutes=crop_growtime)
     plant_time = datetime.datetime.now()
 
     # Calculate harvest duration
     adj_plant_time = plant_time + offset_mins
-    #print("adj_plant_time = plant_time + offset_mins: " + str(plant_time) + " + " + str(offset_mins) + " = " + str(adj_plant_time))
     adj_harvest_time = adj_plant_time + time_change
-    #print("adj_harvest_time = adj_harvest_time + time_change: " + str(adj_harvest_time) + " + " + str(time_change) + " = " + str(adj_harvest_time))
     
     # Round down the harvest time to account for cycles
-    one_min = datetime.timedelta(minutes=1)
     # cycle_time 10 for allotments :00 :10 :20 :30 :40 :50
+    one_min = datetime.timedelta(minutes=1)
     if cycle_time == 10:
         while adj_harvest_time.minute % 10 != 0:
             adj_harvest_time = adj_harvest_time - one_min
     adj_harvest_time = adj_harvest_time.replace(second=0, microsecond=0)
-    #print("Rounded adj_harvest_time: " + str(adj_harvest_time))
 
     actual_growtime = adj_harvest_time - adj_plant_time
-    #print("actual_growtime = adj_harvest_time - adj_plant_time: " + str(adj_harvest_time) + " - " + str(adj_plant_time) + " = " + str(actual_growtime))
     harvest_time = plant_time + actual_growtime
-    #print("harvest_time = plant_time + actual_growtime: " + str(plant_time) + " + " + str(actual_growtime) + " = " + str(harvest_time))
     return harvest_time
 
 def handle_growth_time(n, harvest_time):
     time_now = datetime.datetime.now()
     time_now = time_now.replace(microsecond=0)
+    crop = allotments[n]
 
-    if harvest_time >= time_now:
-        remaining_time = harvest_time - time_now
-        allotments[n]["remaining_label"].configure(text=remaining_time, font='ariel 12', foreground="black")
-        allotments[n]["remaining_label"].after(1000, handle_growth_time, n, harvest_time)
+    if not crop["reset"]:
+        if harvest_time >= time_now:
+            remaining_time = harvest_time - time_now
+            crop["remaining_label"].configure(text=remaining_time, font='ariel 12', foreground="black")
+            crop["remaining_label"].after(1000, handle_growth_time, n, harvest_time)
+        else:
+            crop["label_name"].configure(background="#86d474")
+            crop["active"] = False
     else:
-        allotments[n]["label_name"].configure(background="#86d474")
-        allotments[n]["active"] = False
-
-        
+        crop["reset"] = False
+        crop["active"] = False
 
 def plant(n):
     btn_name = (btn_list[n])
@@ -127,22 +142,26 @@ time_label.pack(fill=tk.X)
 time_frame.pack(fill=tk.X)
 time_label.after(10, flatten_time)
 
-# # Holding the titles / column headers
-# title_frame = tk.Frame(master = window, background="purple")
-# crop_lbl = tk.Label(
-#     master = title_frame,
-#     text = "Crops",
-#     font='ariel 14',
-#     width=25
-# )
-# crop_lbl.pack(side=tk.LEFT)
-# crop_lbl = tk.Label(
-#     master = title_frame,
-#     text = "Harvest",
-#     font='ariel 14',
-# )
-# crop_lbl.pack(side=tk.LEFT)
-# title_frame.pack()
+# under banner -------------------------------------------------------- V
+offset.set(5)
+underbanner_frame = tk.Frame(master = window)
+dropdown_frame = tk.Frame(master = underbanner_frame)
+offset_label = tk.Label(master = dropdown_frame, text = "Set Offset: ")
+offset_label.pack(side=tk.LEFT)
+offset_drop = tk.OptionMenu(dropdown_frame, offset, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+offset_drop.pack(side=tk.LEFT)
+dropdown_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+reset_btn = tk.Button(
+    master = underbanner_frame, 
+    text="Reset", 
+    width=8, height=1, 
+    background="grey",
+    command = partial(reset_crops)    
+)
+reset_btn.pack(side=tk.LEFT, padx=5, pady=5)
+underbanner_frame.pack()
+# under banner -------------------------------------------------------- A
 
 # Holding the grid for the allotments
 main_frame = tk.Frame(master = window)
